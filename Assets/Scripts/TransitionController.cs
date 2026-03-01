70bf61d221352fd53a37aa26a029feaa1fb5327e
MerkuraLampo <123804932+MerkuraLampo@users.noreply.github.com>
2026-03-02 01:26:18 +0900

    1

--- Assets/Scripts/TransitionController.cs
+++ Assets/Scripts/TransitionController.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class TransitionController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public CanvasGroup CanvasGroup;
+    private float FadeSpeed = 1f;
+    private bool isStarting = false;
     void Start()
     {
-        
+        if (CanvasGroup != null)
+        {
+
+            CanvasGroup.alpha = 0;
+            CanvasGroup.interactable = false;
+            CanvasGroup.blocksRaycasts = false;
+        }
     }
 
     // Update is called once per frame
